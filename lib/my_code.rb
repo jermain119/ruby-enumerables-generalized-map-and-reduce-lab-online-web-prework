fb46e882b372d5569727977cf5e55ac85d185afb
joshua richards <jermain119@gmail.com>
2019-09-30 00:32:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(source_array)
+  i= 0
+  negative_array =[]
+while i < source_array.count do
+  negative_array.push(yield (source_array[i]))
+  i += 1
+end
+negative_array
+end 
