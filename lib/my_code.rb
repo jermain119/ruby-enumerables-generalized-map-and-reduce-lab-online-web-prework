ff382b75a70ce0a73da65c71e52fb2d1ff0faee1
joshua richards <jermain119@gmail.com>
2019-09-30 00:33:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ while i < source_array.count do
 end
 negative_array
 end 
+
+def reduce(source_array)
+  i= 0
+  negative_array =[]
+while i < source_array.count do
+  negative_array.push(yield (source_array[i]))
+  i += 1
+end
+negative_array
+end 
