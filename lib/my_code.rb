e8e20da9a850747a9265aac0efa78756d8958e43
joshua richards <jermain119@gmail.com>
2019-09-30 00:52:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,16 @@ negative_array
 end 
 
 def reduce(source_array, sp=0)
-  i= 0
-  sum =sp
-while i < source_array.count do
-  negative_array.push(yield (source_array[i]))
-  i += 1
-end
-negative_array
+  if starting_point
+    total = starting_point
+    i = 0 
+  else 
+    total = source_array[0]
+    i = 1 
+  end
+  while i < source_array.length
+    total = yield(total, source_array[i])
+    i += 1 
+  end
+  total
 end 
